f9fb4d732d76157d75cd004068fe2d87306130f4
ldsgit2406 <Dhanasekar@DESKTOP-RK2B4CQ>
2018-12-25 19:22:18 +0530

    new PATCH /todos/:id api to update todos

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var {mongoose} = require('./db/mongoose.js');
 var {User} = require('./models/user.js');
 var {Todo} = require('./models/todo.js');
 
+const _ = require('lodash');
+
 var app = new express();
 const port = process.env.PORT || 3000;
 
@@ -68,11 +70,39 @@ app.delete('/todos/:id', (req, res) => {
       res.status(404).send();
   })
 
-})
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
+
 
 app.listen(port, () => {
   console.log(`started up at port ${port}`);
-})
+});
 
 module.exports = {app};
 

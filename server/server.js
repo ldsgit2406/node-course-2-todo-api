4da2f803ff737201c3825f090cc592ef5ae54a6f
ldsgit2406 <Dhanasekar@DESKTOP-RK2B4CQ>
2018-12-25 20:35:36 +0530

    create env variable and test database

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env ====',env);
+if(env ==='development'){
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+}else if(env === 'test'){
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
 var express = require('express');
 var bodyParser = require ('body-parser');
 var {ObjectID} = require('mongodb');
